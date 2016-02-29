61bf50272767c1f6f6c2302691d3b9c02c4b7dbe
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-29 18:41:36 -0500

    intitial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+      alert("document ready occurred!");
+});
+
+/*$(window).load(function() {
+      alert("window load occurred!");
+});  -- I'm keeping this here in case it's better. */
+
+//1. Set timer value to "1234" on window load.
+//2. Make a timer that counts down.
+//3.Make click event for countdown.
+//  --- a "click" starts the countdown.
+// 4. runs timer
+//  --- another click stops the countdown.
+// IF there is NO click, timer runs to 0.
+//5. if timer gets to ZERO
+      //then text turns red and timer stops.
+// --- another click RESETS the timer
+ 
