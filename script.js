b400e9e243b04e94ff344c1fe6de26866b8fd242
Charles <charlesthwang@gmail.com>
2016-02-29 23:40:53 -0500

    after counter is clicked, print value of counter every 1 second

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
 $("#counter").click( function(event) {
   event.preventDefault();
   var counterValue = $("#counter").val();
-  console.log(counterValue);
+  setInterval( function() {
+    console.log(counterValue);}, 1000);
+  console.log("I got clicked!");
 });
 
 
