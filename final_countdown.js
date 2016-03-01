0808cda3db4669baef67d07518647d1439a7347b
jblakeman <blakeman.john@gmail.com>
2016-02-29 19:36:53 -0500

    Use object constructor and prototypes

--- final_countdown.js
+++ final_countdown.js
@@ -1,33 +1,46 @@
 $(document).ready(function() {
-    var counter = $("#counter");
-    var startCount = "1234";
-    var started = false;
-    var zeroClass = "fail";
-    var count, queue;
-    counter.val(startCount);
-    function countDown() {
-        count--;
-        if (count === 0) {
-            started = false;
-            counter.addClass(zeroClass);
-            clearInterval(queue);
-        } else {
-            if (counter.hasClass(zeroClass)) counter.removeClass(zeroClass);
-        }
-        counter.val(count);
+    var count, counterID;
+    function Counter(selector, value, failClass, delay) {
+        this.el = $(selector);
+        this.startNum = value;
+        this.count = value;
+        this.started = false;
+        this.failClass = failClass;
+        this.delay = delay;
     }
-    counter.on("click", function(event) {
-        event.preventDefault();
-        if (started) {
-            started = false;
-            clearInterval(queue);
+    Counter.prototype.countDown = function() {
+        if (this.count === 0) {
+            this.started = false;
+            this.el.addClass(this.failClass);
+            this.stopCounting();
         } else {
-            started = true;
-            count = startCount;
-            counter.val(startCount);
-            // The following should behave the same as setting the delay to 0
-            // in browsers that follow the HTML5 spec.
-            queue = setInterval(countDown, 4);
+            this.count--;
+            this.el.val(this.count);
+            if (this.el.hasClass(this.failClass)) {
+                this.el.removeClass(this.failClass);
+            }
+        }
+        this.el.val(this.count);
+    };
+    Counter.prototype.startCounting = function() {
+        this.id = setInterval(this.countDown.bind(this), this.delay);
+    };
+    Counter.prototype.stopCounting = function() {
+        this.started = false;
+        if (this.id) clearInterval(this.id);
+    };
+    var counter = new Counter("#counter", "1234", "fail", 4);
+    counter.el.val(counter.startNum);
+    counter.el.on("click", function(event) {
+        event.preventDefault();
+        if (counter.started) {
+            counter.started = false;
+            counter.stopCounting();
+        } else {
+            counter.started = true;
+            counter.count = counter.startNum;
+            counter.el.val(counter.startNum);
+            counter.startCounting();
         }
     });
 });
