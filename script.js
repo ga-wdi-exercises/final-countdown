d88c53829d70ee001db0573056cf28f07417b045
Andrew Smith-Mui <arsm800@gmail.com>
2016-03-01 13:49:19 -0500

    countdown initialized

--- script.js
+++ script.js
@@ -22,18 +22,29 @@
 
 // - After the counter is clicked, every 1 second decrease the original value of the counter by 1 and replace the counter's text with the new value.
 
+// - Stop the timer.
+
 var form = $("#counter");
-var counter = form.val();
+var counter = parseInt(form.val());//If this is a string since it is in HTML, do we need a parseInt()?
+
+//var decreaseValue = function() {  Trying .prop() instead. form.val() vs. prop("value")?
+  //counter -=1;
 
 var countdownValue = function () {
   console.log(counter);
 };
 
 var decreaseValue = function() {
-  console.log(counter -=1);
+  form.prop("value", counter -= 1);
 };
 
-form.on("click", function(evt) {
+form.on("click", function(evt) {     //You can also write this as form.onClick();
   evt.preventDefault();
   setInterval(decreaseValue, 1000);
 });                                 //As long as you have your javascript script.js link in the body of the HTML doc, you should not have to include your entire code in a document.ready(function());
+
+
+//When time = 0, class = fail.  This is based on HTML from solution as site runs.
+//$("#counter").prop("value") = number from where counter starts countdown.
+
+//
