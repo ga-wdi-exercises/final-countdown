f421f719b9a2962fbe1f9fb0802a58a2288b6e9f
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-01 10:02:22 -0500

    Nicks Review Notes - added mess

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 $(document).ready(function() {
-      alert("document ready occurred!");
-});
+$("title").html("AW Final Countdown")
 
-/*$(window).load(function() {
-      alert("window load occurred!");
-});  -- I'm keeping this here in case it's better. */
 
-//1. Set timer value to "1234" on window load.
+
+}); 
+
+// var firstNum = parseInt($(counter.val)()
+
+//
+// var timerIsRunning = false;
+// var timerId
+//
+// function minuseOne(){
+//   $("#counter").removeClass("fail")
+//   $("#counter").prop("value",firstNum)
+// } if (firstNumer is greather than zeore the nthe first numer = first numerb it one )
+//
+//
+// $("#counter").click(function(evt){
+//   event.preventDefault();
+//   if (timerIsRunning == false){
+//     timerId-setInterval(minusOne, 1000)
+//     console.log("secondtimer",timerId)
+//     isTimerRunning = true
+//   }else if (isTimerRunning == true) {
+//     clearInterval(timerId)
+//     isTimerRunning == false;
+//     console.log(isTimerRunning)
+//     counter.val(default)
+// })
+
+// /// re counter: .on("click", #counter)
+//       - set inerval
+//       - speed
+//       -decrement - decrease time
+//
+// - reset
+//
+//         - gets to zero - gets a class of fail (watch the orignal html as you go)
+
 //2. Make a timer that counts down.
 //3.Make click event for countdown.
 //  --- a "click" starts the countdown.
@@ -16,4 +48,10 @@ $(document).ready(function() {
 //5. if timer gets to ZERO
       //then text turns red and timer stops.
 // --- another click RESETS the timer
- 
+
+
+
+
+/*$(window).load(function() {
+      alert("window load occurred!");
+});  -- I'm keeping this here in case it's better. */
