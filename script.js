38cc1e9bc8c2d49acdb88cedb7093071e57da174
Charles <charlesthwang@gmail.com>
2016-02-29 23:26:17 -0500

    print current value to console

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+
+$("#counter").click( function(event) {
+  event.preventDefault();
+  var counterValue = $("#counter").val();
+  console.log(counterValue);
+});
+
+
+
+
+
+
+
+
+
+
+
+});
