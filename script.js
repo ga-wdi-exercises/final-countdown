4ddd3ef071bb058cfe55da1755332831d2974cab
Charles <charlesthwang@gmail.com>
2016-03-01 14:03:28 -0500

    added start/pause functionality to timer

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 $(document).ready(function() {
 
+// defining a variable to the timer
+var timerID;
 
+// defining a boolean variable to when timer is 'on'
+var timerRunning = false;
+
+// set counter to 1234
+var counterValue = 1234;
+$("#counter").val(counterValue);
+
+// function for decreasing timer
+function countDown() {
+    counterValue = counterValue - 1;
+    $("#counter").val(counterValue);
+    console.log(counterValue);
+}
+
+
+// when counter is clicked, start countdown
 $("#counter").click( function(event) {
   event.preventDefault();
-  var counterValue = $("#counter").val();
-  setInterval( function() {
-    console.log(counterValue);}, 1000);
-  console.log("I got clicked!");
-});
+// decrease timer by one second by calling countDown function here
+// reassigns timerRunning to true to stop timer
+    if (timerRunning == false) {
+      timerID = setInterval(countDown, 10);
+      timerRunning = true;
+      console.log(counterValue);
+      console.log("I got clicked!");
+// stops timer and reassigns timerRunning to false to continue timer     
+      } else if (timerRunning == true) {
+        clearInterval(timerID);
+        timerRunning = false;
+        }
+    });
 
 
+
+
+
+// when counter is clicked, stop timer
+// $("#counter").click(function() {
+//   clearInterval(timerID);
+// });
 
 
 
