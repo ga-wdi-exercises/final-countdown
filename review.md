2d124585fbba7f36bf2c880f988f174155f8aaf8
Hilary Masland <hilarymasland@gmail.com>
2016-03-01 09:45:43 -0500

    Intial commit - early edits

---
+++ review.md
@@ -0,0 +1,9 @@
+function minusOne(){
+$("#counter")).removeClass("fail");
+$("#counter").prop("value", firstNum);
+
+if(firstNum > 0){
+    firstNum=firstNum-1;
+
+}
+}
---
+++ script.js
@@ -0,0 +1,21 @@
+//Create a counter
+  //Display 1234
+    //counter function
+    var counter = #counter
+      $("#counter").click(function(evt)
+      console.log("yay");
+      }
+
+
+//if clicked
+    //decrement to zero
+    //when zero, turn red
+    //class fail
+//Second click RESET back to 1234
+
+
+
+
+//$("#counter").click(function(evt)){
+//  evt.preventDefault():
+//}
