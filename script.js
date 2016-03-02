47c608402c8d52d3a6ef95feed2276866f4c984c
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-01 20:07:14 -0500

    timer is working now - thank you joe for help

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 $(document).ready(function() {
-
+  $("title").html("AW Final Countdown")
   $("#counter").val("1234");
 
-var counter =$("#counter");
+// var counter =$("#counter");
 var counterDefault =parseInt($("#counter").val());
 var theTimerIsRunning = false;
-var time = 0
-var timerId;
+var currentTimerId;
+
+function decrease(){
+  counterDefault--
+  $("#counter").val(counterDefault);
+}
 
 function countdown(){
   $("#counter").click(function(event){
-    event.preventDefault();
-    if(theTimerIsRunning===false){
-      timerId= setInterval(countdown,1000);
+      event.preventDefault();
+      console.log("Adrian")
+    if(theTimerIsRunning === false){
+      currentTimerId = setInterval(decrease,1000);
       theTimerIsRunning = true;
-    }else if(theTimerIsRunning === true){
-      clearInterval(timerId)
+    }
+    else if(theTimerIsRunning === true){
+      clearInterval(currentTimerId);
       theTimerIsRunning = false;
-    }})}
-
+    }
+  })
+}
+countdown();
 });
