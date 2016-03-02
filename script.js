7bbe730ea2d21ba8739429340c11830d19084024
Hilary Masland <hilarymasland@gmail.com>
2016-03-02 08:31:39 -0500

    Updates

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-//Create a counter
-  //Display 1234
-    //counter function
-    var counter = #counter
-      $("#counter").click(function(evt)
+
+$(document).ready(function){
+#("#counter").val(1234);
+
+var counter = $("#counter");
+var timerId;
+var isCountingDown = false;
+
+
+  //Create a counter
+    //Display 1234
+      //counter function
+
+    counter.on("click", function(event) {
+      event.preventDefault();
       console.log("yay");
       }
-
-
+}
+}
 //if clicked
     //decrement to zero
     //when zero, turn red
     //class fail
 //Second click RESET back to 1234
-
+//counter.val(1234)
 
 
 
