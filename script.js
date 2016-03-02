297519fd70e9638437a0ffa390e192aa266223f7
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-01 19:18:48 -0500

    working with Lyndsey

--- script.js
+++ script.js
@@ -1,57 +1,22 @@
 $(document).ready(function() {
-$("title").html("AW Final Countdown")
 
+  $("#counter").val("1234");
 
+var counter =$("#counter");
+var counterDefault =parseInt($("#counter").val());
+var theTimerIsRunning = false;
+var time = 0
+var timerId;
 
-}); 
+function countdown(){
+  $("#counter").click(function(event){
+    event.preventDefault();
+    if(theTimerIsRunning===false){
+      timerId= setInterval(countdown,1000);
+      theTimerIsRunning = true;
+    }else if(theTimerIsRunning === true){
+      clearInterval(timerId)
+      theTimerIsRunning = false;
+    }})}
 
-// var firstNum = parseInt($(counter.val)()
-
-//
-// var timerIsRunning = false;
-// var timerId
-//
-// function minuseOne(){
-//   $("#counter").removeClass("fail")
-//   $("#counter").prop("value",firstNum)
-// } if (firstNumer is greather than zeore the nthe first numer = first numerb it one )
-//
-//
-// $("#counter").click(function(evt){
-//   event.preventDefault();
-//   if (timerIsRunning == false){
-//     timerId-setInterval(minusOne, 1000)
-//     console.log("secondtimer",timerId)
-//     isTimerRunning = true
-//   }else if (isTimerRunning == true) {
-//     clearInterval(timerId)
-//     isTimerRunning == false;
-//     console.log(isTimerRunning)
-//     counter.val(default)
-// })
-
-// /// re counter: .on("click", #counter)
-//       - set inerval
-//       - speed
-//       -decrement - decrease time
-//
-// - reset
-//
-//         - gets to zero - gets a class of fail (watch the orignal html as you go)
-
-//2. Make a timer that counts down.
-//3.Make click event for countdown.
-//  --- a "click" starts the countdown.
-// 4. runs timer
-//  --- another click stops the countdown.
-// IF there is NO click, timer runs to 0.
-//5. if timer gets to ZERO
-      //then text turns red and timer stops.
-// --- another click RESETS the timer
-
-
-
-
-/*$(window).load(function() {
-      alert("window load occurred!");
-});  -- I'm keeping this here in case it's better. */
+});
