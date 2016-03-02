502e6d6978409da7b07fedc91181d9cc8aaf5784
Charles <charlesthwang@gmail.com>
2016-03-01 19:26:08 -0500

    cleaned up code with Jesses help, MIND BLOWN

--- script.js
+++ script.js
@@ -2,44 +2,41 @@ $(document).ready(function() {
 
 // defining a variable to the counter
 var counterID;
+var $counter = $("#counter");
 
-// defining a boolean variable to when counter is 'on'
-var counterRunning = false;
-
-// set counter to 1234
-var counterValue = 10;
-$("#counter").val(counterValue);
+// defining variable for value of counter set in HTML
+var counterValue = parseInt($counter.val());
 
 // function for decreasing counter
-// added code so if counterValue is zero, counter stops
+// added code so if counterValue is zero, counter stops, resets to 200 upon click
 function countDown() {
+  // this is same as counterValue = counterValue - 1;
+  $counter.val(--counterValue);
+  console.log(counterValue);
   if (counterValue == 0) {
-    clearInterval(counterID);
-    $("#counter").addClass("fail");
-  } else if (counterValue <= 10) {
-      counterValue = counterValue - 1;
+    counterID = clearInterval(counterID);
+    // counterValue = 200;
+    // turn counter red when counter hits zero
+    $counter.addClass("fail");
   }
-    $("#counter").val(counterValue);
-    console.log(counterValue);
 }
 
 
 // when counter is clicked, start countdown
-$("#counter").click( function(event) {
+$counter.click( function(event) {
   event.preventDefault();
-// decrease counter by one second by calling countDown function here
-// reassigns counterRunning to true to stop timer
-    if (counterRunning == false) {
-      counterValue = 10;
-      $("#counter").removeClass("fail");
-      counterID = setInterval(countDown, 200);
-      counterRunning = true;
+// decrease counter by one second upon click by calling countDown function here
+// counterID here is 'falsey', so jump to 'else'
+    if (counterID) {
+      counterID = clearInterval(counterID);
+    } else {
+// resets counter to 200 when clicked again
+      counterValue = 200;
+// removes .fail class to turn counter back to green when counter restarts
+      $counter.removeClass("fail");
+      counterID = setInterval(countDown, 10);
       console.log(counterValue);
       console.log("I got clicked!");
-// stops counter and reassigns timerRunning to false to continue timer
-    } else if (counterRunning == true) {
-        clearInterval(counterID);
-        counterRunning = false;
     }
 });
 
