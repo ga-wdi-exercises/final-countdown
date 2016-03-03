7f5e6c3e1cd25e10d191190cabdbd5ebf4054bc7
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-02 19:59:57 -0500

    stopped the timer and turned it red

--- script.js
+++ script.js
@@ -2,28 +2,37 @@ $(document).ready(function() {
   $("title").html("AW Final Countdown")
   $("#counter").val("1234");
 
-// var counter =$("#counter");
-var counterDefault =parseInt($("#counter").val());
-var theTimerIsRunning = false;
-var currentTimerId;
+  var counterDefault =parseInt($("#counter").val());
+  var theTimerIsRunning = false;
+  var currentTimerId;
 
-function decrease(){
-  counterDefault--;
-  $("#counter").val(counterDefault);
-}
-
-function countdown(){
-  $("#counter").click(function(event){
-      event.preventDefault();
-      console.log("Adrian")
-    if(theTimerIsRunning === false){
-      currentTimerId = setInterval(decrease);
-      theTimerIsRunning = true;
-    }
-    else if(theTimerIsRunning === true){
+  function decrease(){
+    if (counterDefault<=0){
       clearInterval(currentTimerId);
-      theTimerIsRunning = false;
+      console.log(currentTimerId);
+      $("#counter").css("color","red");
+      $("#counter").css("box-shadow","0px 0px 100px 0px red");
     }
+    else{counterDefault--;
+      $("#counter").val(counterDefault);
+      console.log(counterDefault)
+    }
+  }
+
+  function countdown(){
+    $("#counter").click(function(event){
+      event.preventDefault();
+      if(theTimerIsRunning === false){
+        currentTimerId = setInterval(decrease);
+        theTimerIsRunning = true;
+        console.log("hey");
+      }
+      else if(theTimerIsRunning === true){
+        clearInterval(currentTimerId);
+        theTimerIsRunning = false;
+      }
+    })
+
   })
 }
 countdown();
