1c1f3ae9be55409b03527478bb6618e6495f376f
Woo Yun <hh747hh@gmail.com>
2016-03-03 06:15:31 -0500

    it still doesn't work

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 $(document).ready(function(){
 
-var counterID;
-var $counter = $("#counter");
-var counterValue = parseInt($counter.val());
+//defining variable
+var counterID == null;
 
-function counDown() {
-$counter.val(--counterValue);
-console.log(counterValue);
+//value of counter to numbers
+var counterValue = parseFloat($("counter").val());
+
+//set coundown function
+// when it reaches 0, add class fail
+function countDown() {
+$("counter").val(--counterValue);
 if (counterValue == 0){
   counterID = clearInterval(counterID);
-$counter.addClass("fail");
+$("counter").addClass("fail");
   }
 }
+
+//when clicked, countdown starts
+$("counter").click( function(event) {
+event.preventDefault();
+if (counterID == null) {
+  $("counter").removeClass("fail");
+  counterID = setInterval(countDown, 1000)
+} else {
+  counterValue == "1234"
+}
+}
+});
