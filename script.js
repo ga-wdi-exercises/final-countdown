1415f9bc95e5fb43444b3f50301dce0c1df420cd
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-03-03 08:49:15 -0500

    final attempt before pull request

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
-$(document).ready(function(){
-  // counter.val(1234);
+$(document).ready(function() {
+  $("title").html("AW Final Countdown")
+  $("#counter").val("1234");
 
+// var counter =$("#counter");
+var counterDefault =parseInt($("#counter").val());
+var theTimerIsRunning = false;
+var currentTimerId;
 
-var time = 0;
-var timer;
-var runningTime = false;
-var timeDefault = counter.val();
-var counter= $("#counter");
-var timerId;
-____________________________________________________________
-// a counter that countes down
+function decrease(){
+  counterDefault--;
+  $("#counter").val(counterDefault);
+}
 
-// $("#counter").click(function(event){
-  // event.preventDefault();
-    // if
-
-// }
-
-
-function countDown (){
+function countdown(){
   $("#counter").click(function(event){
-    event.preventDefault();
-    if (theTimerisRunning === false){
-      timerId= setInterval(countDown, 1000);
-      theTimerisRunning = true;
-    } else if (theTimerisRunning === true){
-      clearInterval(timerId)
-      theTimerisRunning= false;
-        }
-
-      }
-
+      event.preventDefault();
+      console.log("Adrian")
+    if(theTimerIsRunning === false){
+      currentTimerId = setInterval(decrease);
+      theTimerIsRunning = true;
     }
-
-  }
-  ________________________________________________________________
-  // set click fucntion
-
-
-  
+    else if(theTimerIsRunning === true){
+      clearInterval(currentTimerId);
+      theTimerIsRunning = false;
+    }
+  })
+}
+countdown();
 });
