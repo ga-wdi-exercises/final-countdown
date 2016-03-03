a5b67510ee4c3f140125f1a9a48fc9c03ff680a3
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-03 01:58:38 -0500

    able to get set up with timer moving slowly

--- script.js
+++ script.js
@@ -1,97 +1,31 @@
 $(document).ready(function(){
-  $("#counter").click(function(evt) {
-    evt.preventDefault();
-  })
-var countval = 1234;
-var counter = setIntreval(timer,1000);
+$("#counter").val("1234");
 
-function timer(){
-  count += 1;
-  if(countval ===0){
-    clearInterval(counter);
+var counterValue = parseInt($("#counter").val());
+var timer = false;
+var timerId;
+
+function countdown(){
+  if (counterValue <=0){
+    clearInterval(timerId);
+    $("#counter").css("color","red");
   }
+  else{counterValue--;
+  $("#counter").val(counterValue);
+  }
+}
+function countdownClick(){
+  $("#counter").click(function(evt){
+    evt.preventDefault();
+    if(timer === false){
+      timerId = setIntreval(countdown);
+      timer = true;
+    }
+    else if (timer === true){
+      clearInterval(timerId);
+      timer= false;
+    }
+  })
 })
-})
-  // var value = "1234"
-  // var counter = $("#counter")
-  // var timer =setIntreval(timer,1000)
-
-  //   var timer = setInterval(function() {
-  //
-  //     var count = parseInt($("#counter").html());
-  //       $("#counter").on("click", function(){
-  //         if (count !== 0) {
-  //           $("#counter").html("1234" - 1);
-  //         } else {
-  //           clearInterval(timer);
-  //         }
-  //       }, 1000);
-  //       })
-  //
-  // });
-
-//   var countdown =function(){
-//     $('#counter').each(function() {
-//       var count = parseInt($(this).html());
-//       if (count!== 0){
-//         $(this).html(count-1);
-//       }
-//     });
-//   };
-//   setInterval(countdown, 1000);
-// })
-
-  // $("#counter").on("click", function(){
-  //   startTimer = setIntreval(1000);
-  // });
-  //
-  //   this.counter.on("click", function(){
-  //     stopTimer = clearInterval("#counter");
-  //   });
-  // clearInterval("#counter")
-  //     }
-
-// function Countdown() {
-// value = count - 1;
-// if (count<=0)
-// {
-//   this.css("color", "red")
-//   clearInterval(counter);
-//   return;
-// }
-// }
-
-
-
-//
-//   this.counter.on("click", function(){
-//     stopTimer = clearInterval("#counter");
-//   });
-// clearInterval("#counter")
-//     }
-//   }
-  //   this.css("color", "green")
-  //   this.css("background-color", "white")
-  //   this.addClass("greenified")
-  //   return this
-  // }
-
-// var
-// form
-// #counter
-//       #counter:focus
-//       #counter:hover
-//       #counter.fail
-//timer
-//
-//
-// on load, counter shows set at 1234
-// on click start
-// number decrease
-// speed increases as time goes by
-// onclick stop
-// show number
-// clear and restart at 1234
-//
-//
-// var start
+countdownClick();
+}
