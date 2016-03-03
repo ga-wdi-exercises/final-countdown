f1e4597f74efe30e06a990af2fbc46cb44717af4
BrittanyBranson <brittanydibble@Brittanys-MacBook-Pro.local>
2016-03-02 21:33:20 -0500

    second attempt at final countdown game

--- script.js
+++ script.js
@@ -1,74 +1,50 @@
-//page loads with a timer of 1234.
+//page load
+$(document).ready(function() {
+  $("title").html("final countdown")
+//place the value of the counter into the HTML
+  $("#counter").val("1234");
 
-//mouseover color effect - text opacity changes
+//that value becomes the default
+//
+  var counterDefault = parseInt($("#counter").val());
+  var timerRunning = false;
+  var currentTimer;
 
-//event listener - clicks- begins descending countdown
-
-//if value is less than 0, stop
-
-//text is red
-
-//else reset
-
-//event listener- click- pauses/stops the timer
-
-//WHAT WE NEED FOR COUNTDOWN
-//status: is running
-//on click function: #counter
-//set interval: speed, and function that descreases in time
-//reset
-//when time == 0, add class fail
-
-//parsefloat adds a floating decimal. does both its job and parseInt
-
-$(document).ready(function(){
-
-  var counter = $("#counter")
-  //var firstNum = parseInt.counter.val //parseInt ensures that .val returns a number, not a string
-  //parse the value as an integer
-  var firstNum = $("#counter").val()
-
-  //need a state to compare things to for click function as learned in project 1 with adding an active class
-  var isCountingDown = false;
-  var timerId;
-  //var default = 10;
-
-  function minusOne(){
-    counter.removeClass("fail")
-    //$("#counter").removeClass("fail")
-    $("#counter").prop("value", firstNum);
-    //$("#counter").val(firstNum)
-
-    //decrease original num by 1
-    //on page load firstNum == 1234
-    if(firstNum >  0){
-      firstNum = firstNum-1;
-    //if number is 0 or less than stop the timer
-  }else if(firstNum <= 0){
-    //if the couner reaches 0, counter's text should turn red
-    counter.val("10")
-    $("#counter").addClass("fail")
-    console.log("addedFailOnClass") //we need to clearn the interval and re-set it back!!!
+//function decrease
+  function decrease() {
+//if the value of counterDefault is less than or equal to 0, the current timer is "cleared" (meaning the text becomes red to indicate loss)
+    if(counterDefault <= 0) {
+      clearInterval(currentTimer);
+//coloring the counter
+    $("#counter").css("color", "red");
+    $("#counter").css("box-shadow", "0px 0px 100px 0px red");
+    }
+//subtracts 1 from the value of class "#counter"
+    else{counterDefault--;
+    $("#counter").val(counterDefault);
   }
 }
-
-$("#counter").on("click", function(evt){
-  //console.log("first", timerId);
-    evt.preventDefault(); //prevent default behavior
-    if(isCountingDown == false) { //if the timer is not running, and there's a click
-      timerId = setInterval(minusOne, 1000) //start the timer!
-      console.log("second", timerId);
-      isCountingDown = true //the next time the user clicks
-    }else if (isCountingDown == true) {
-      clearInterval(timerId)
-      isCountingDown = false //change value back to false, so that another click will re-set the interval
-      console.log(isCountingDown);
-      //update the display with default
-      //counter.val(firstNum)
-    }else if(counter.val() <= 0) {
-      console.log("timer0")
-      $("#counter").removeClass("fail") //it's not executing! whyyyyyyy
-      counter.val("10")
-    }
-  });
-})
+//function countdown
+  function countdown() {
+//assigns a click event to the counter
+    $("#counter").click(function(evt) {
+//still confused about the prevent default, but I know it prevents the browser from having to be refreshed after every click event
+      evt.preventDefault();
+//if the timer is NOT currently running
+      if(timerRunning === false) {
+//run the function descrease on the currentTimer UNTIL...
+        currentTimer = setInterval(decrease);
+//the timer IS currently running...so STOP running that decrease function
+        timerRunning = true;
+      }
+//if the timer IS currently running
+      else if(timerRunning === true) {
+//
+        clearInterval(currentTimer);
+        timerRunning = false;
+      }
+    })
+  })
+}
+countDown();
+});
